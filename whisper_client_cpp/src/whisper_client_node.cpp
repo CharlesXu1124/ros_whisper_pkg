c17a8a91b9f21fc3f59ef6432c7302f90fc1da19
charlesxu1124 <charles.xu@upower.com>
2023-05-26 18:43:02 -0700

    added string variable

--- whisper_client_cpp/src/whisper_client_node.cpp
+++ whisper_client_cpp/src/whisper_client_node.cpp
@@ -7,12 +7,16 @@ namespace whisper_client
 WhisperClientNode::WhisperClientNode(const rclcpp::NodeOptions & options) : rclcpp::Node("whisper_client_node", options)
 {
   pub_posecov_stamp_ptr = this->create_publisher<std_msgs::msg::String>("whisper_inference_results", rclcpp::QoS{10});
-
+  timer_ = this->create_wall_timer(
+      3000ms, std::bind(&WhisperClientNode::call_whisper_server, this));
 
 }
 
 void WhisperClientNode::call_whisper_server()
 {
+  auto message = std_msgs::msg::String();
+  message.data = "2";
+  pub_posecov_stamp_ptr->publish(message);
 
 }
 
